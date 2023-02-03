f0161d096331a3a5c7a2cee246e65bda2e06db92
Abhishek Bohora <abhishek.bohora16@gmail.com>
2023-02-03 18:55:34 +0545

    created jwt token generating function

---
+++ BasicCrud/Controllers/AuthenticationController.cs
@@ -0,0 +1,100 @@
+﻿using BasicCrud.Configurations;
+using BasicCrud.Models;
+using BasicCrud.Models.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BasicCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly JwtConfig _jwtConfig;
+
+        public AuthenticationController(UserManager<IdentityUser> userManager, JwtConfig jwtConfig)
+        {
+            _userManager = userManager;
+            _jwtConfig = jwtConfig;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] UserRegistrationRequestDto requestDto) {
+            //validate the incoming request
+            if(ModelState.IsValid)
+            {
+                var user_exists = await _userManager.FindByEmailAsync(requestDto.Email);
+                if(user_exists != null) {
+                    return BadRequest(new AuthResult()
+                    {
+                        Result = false,
+                        Errors = new List<string>()
+                        {
+                            "Email already exists"
+                        }
+                    });
+                }
+                //creating a new user
+                var new_user = new IdentityUser() {
+                    Email = requestDto.Email,
+                    UserName = requestDto.Email
+                };
+
+                var is_created = await _userManager.CreateAsync(new_user, requestDto.Password);
+                if(is_created.Succeeded) {
+                    //Generate the token 
+                    var token = generateJwtToken(new_user);
+                    return Ok(new AuthResult()
+                    {
+                        Result = true,
+                        Token = token
+                    });   
+                
+                };
+                return BadRequest(new AuthResult() {
+                    Result= false,
+                    Errors = new List<string>()
+                    {
+                        "Server error"
+                    }
+                });
+            }
+            return BadRequest();
+        }
+
+        private string generateJwtToken(IdentityUser user)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_jwtConfig.Secret);
+
+            //Token descriptor
+            var tokenDescriptor = new SecurityTokenDescriptor() {
+                Subject = new ClaimsIdentity(new[] {
+                    new Claim("Id", user.Id),
+                    new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                    new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Iat, DateTime.Now.ToUniversalTime().ToString()),
+                }),
+
+                Expires = DateTime.Now.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),SecurityAlgorithms.HmacSha256)
+            
+            };
+
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+            var jwtToken = jwtTokenHandler.WriteToken(token);
+            return jwtToken;
+        }
+
+    }
+}
+
+

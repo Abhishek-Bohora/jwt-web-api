67ada49dc41d440752e2fd4b7d5748d7883a0aac
Abhishek Bohora <abhishek.bohora16@gmail.com>
2023-02-03 12:03:26 +0545

    added identity and migration

--- BasicCrud/Data/ApplicationDbContext.cs
+++ BasicCrud/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using BasicCrud.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BasicCrud.Data
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext:IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
         {
